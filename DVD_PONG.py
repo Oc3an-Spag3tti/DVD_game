58f63dc09cf6f230769223ba71b6888ad9cc46a2
Oc3an-Spag3tti <49808789+Oc3an-Spag3tti@users.noreply.github.com>
2021-12-26 14:30:56 +0100

    Add files via upload

---
+++ DVD_PONG.py
@@ -0,0 +1,135 @@
+import pygame
+from pygame.locals import * 
+
+pygame.init()
+
+screen_width = 1920
+screen_height = 1080
+full_hd = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('DVD PONg')
+
+background = (0, 0, 0)
+
+brick_lvl_3 = (0,102,128)
+brick_lvl_2 = (0,184,230)
+brick_lvl_1 = (77,219,255)
+
+
+player_color =  (115, 115, 115)
+player_border = (179, 179, 179)
+
+
+  
+columns = 6
+rows = 6
+clock = pygame.time.Clock()
+fps = 60
+
+class edge():
+    def __init__(self):
+        self.width = screen_width // columns
+        self.height = 50
+
+    def create_edge(self):
+        self.bricks = []
+
+        brick_id = []
+        for row in range(rows):
+
+            brick_row = []
+            for column in range(columns):
+
+                brick_x = column * self.width
+                brick_y = row * self.height
+                rectangle = pygame.Rect(brick_x, brick_y, self.width, self.height)
+
+                if row < 2:
+                    hp = 3 
+                elif row < 4:
+                    hp = 2
+                elif row < 6:
+                    hp = 1
+                
+                brick_id = [rectangle, hp]
+
+                brick_row.append(brick_id)
+
+                self.bricks.append(brick_row)
+
+    def appear_edge(self):
+        for row in self.bricks:
+            for brick in row:
+
+                if brick[1]  == 3:
+                    brick_color = brick_lvl_3
+
+                elif brick[1]  == 2:
+                    brick_color = brick_lvl_2
+
+                elif brick[1]  == 1:
+                    brick_color = brick_lvl_1
+
+                pygame.draw.rect(full_hd, brick_color,brick[0])
+                pygame.draw.rect(full_hd, background,brick[0], 2)
+
+class player():
+    def __init__(self):
+        self.height = 20
+        self.width = int(screen_width / columns)
+        self.x = int((screen_width / 2) - (self.width / 2))
+        self.y = screen_height - (self.height * 2)
+        self.speed = 13
+        self.rectange = Rect(self.x, self.y, self.width, self.height)
+        self.direction = 0
+
+    def move(self):
+
+        self.direction = 0
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and self.rectange.left > 0:
+            self.rectange.x -= self.speed
+            self.direction = - 1
+        if key[pygame.K_RIGHT] and self.rectange.right < screen_width:
+            self.rectange.x += self.speed
+            self.direction = 1
+
+    def draw(self):
+        pygame.draw.rect(full_hd, player_color, self.rectange)
+        pygame.draw.rect(full_hd, player_border, self.rectange, 3)
+
+class ball():
+    def __init__(self, x, y):
+        self
+
+
+
+Edge = edge()
+Edge.create_edge()
+
+player_paddle = player()
+
+
+
+
+
+
+
+running = True
+while running:
+
+    clock.tick(fps)
+    full_hd.fill
+
+    full_hd.fill(background)
+     
+    Edge.appear_edge()
+
+    player_paddle.draw()
+    player_paddle.move()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.update()
+
---
+++ Readme_DVD.md
@@ -0,0 +1,14 @@
+﻿----
+ DVD PONG
+---
+**Bienvenue** a mon projet **DVD PONG**
+L'idee de projet est de creer un jeu pong, l'objectif de ce jeu c'est que le logo
+**DVD** detruit toutes les briques et a chaque frappe d'un brique le logo changera sa couleur. Les briques aurons des point de vies on peut observer par la sa nuance.
+
+- BONUS - Si j'aurai le temps je peut rajouter des sons et le menu de demarage du jeu, et peut etre le score avec game over.
+- **PROBLEME PRINCIPAL**  ---> j'ai rencontre un probleme avec mon logo dvd, j'ai du mal a placer le sprite, a la position du joueur pourtant la position de sprite semble logique de plus je sais pas comment et quelle dimension pour le logo, et finalement comment je vais gerer que elle change la couleur lors d'une brique ou sur le bord de l'ecran.
+- **Probleme secondaire** ---> C'est les collision mais je pense ca va marcher mes fichier prototypes precedents (j'ai le code) ou j'avais juste logo dvd qui faisait rebondir sur les bords de l'ecran.
+---
+*Pour rajouter*---> J'avais plusieur concepts en tete pour le jeu, et quand j'etait a la moitie de mon chemin sur ma idee precedente j'ai compris que mon idee en realite ne marche pas ou marche tres mal, finalement j'ai decide de faire un truc plus classique car inventer sa propre mecanique du jeu c'est beaucoup plus dur que de faire un jeu avec une idee similaire.
+    
+
